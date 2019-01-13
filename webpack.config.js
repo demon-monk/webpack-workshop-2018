6b5ebab7c905f9fdf7565e983dad13c0241878ac
demon-monk <antdude@126.com>
2019-01-13 17:13:14 +0800

    ✨ Feat: add two plugins: html-webpack-plugin, webpacl.ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = ({ mode }) => {
   return {
     mode,
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
