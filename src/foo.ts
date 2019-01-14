b2ef7d15ce1e4c0714166771de9ade8b9da2f0e0
demon-monk <antdude@126.com>
2019-01-14 21:52:26 +0800

    feat: 🎸 demonstrate webpack presets with ts support

---
+++ src/foo.ts
@@ -0,0 +1,3 @@
+export function foo (text: string): string {
+    return `${text}-foo`
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { nav } from './nav';
 import { footer } from './foot';
+import {foo} from './foo.ts'
 import imgUrl from './icon.jpg';
 import makeImg from './image';
+
+console.log(foo('test presets'));
 
 document.body.appendChild(nav);
 document.body.appendChild(footer);
