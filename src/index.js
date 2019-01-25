00d5b3ce56389e2b53fb59dd0627c9d1ef96fcda
demon-monk <antdude@126.com>
2019-01-25 22:42:39 +0800

    ✨ Feat: dynamic load named export from library to minimize bundle as far as possible

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // import * as gsap from 'gsap'
 import { nav } from './nav';
-import {foo} from './foo.ts'
+import { foo } from './foo.ts';
 import imgUrl from './icon.jpg';
 import makeImg from './image';
 
-const getFooter = () => import('./foot')
-const getGSAP = () => import('gsap')
-
+const getFooter = () => import('./foot');
+const getGSAP = () => import('gsap');
+const getUniq = () => import('lodash-es/uniq');
 console.log(foo('test presets'));
 
 document.body.appendChild(nav);
 nav.addEventListener('click', () => {
-    getFooter().then(defaultModule => {
-        document.body.appendChild(defaultModule.footer);
-    })
-    getGSAP().then((gsap) => {
-        console.log(gsap)
-    })
-})
+  getFooter().then(defaultModule => {
+    document.body.appendChild(defaultModule.footer);
+  });
+  getGSAP().then(gsap => {
+    console.log(gsap);
+  });
+});
 document.body.appendChild(makeImg(imgUrl));
